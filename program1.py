0b96ce9fdbd2ba263c107864928c1612ec50aa4f
ninad <73416405+ninadm1887@users.noreply.github.com>
2022-02-07 00:24:02 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,33 @@
+import csv
+import math
+
+with open('data.csv', newline='') as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+
+data_file = file_data[0]
+
+def mean(data):
+
+    n = len(data)
+    total = 0
+    for x in data:
+        total += int(x)
+
+    mean = total / n
+    return mean
+
+squared_list = []
+for number in data_file:
+    a = int(number) - mean(data_file)
+    a = a**2
+    squared_list.append(a)
+
+sum = 0
+for i in squared_list:
+    sum += i
+
+result = sum/(len(data_file)-1)
+
+std_deviation = math.sqrt(result)
+print(std_deviation)
